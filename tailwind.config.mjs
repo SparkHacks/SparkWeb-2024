2b9068e778c23167ad1425122e147f131a5360cb
floatsinkfc <floatsinkfc@gmail.com>
2023-12-20 23:22:12 -0600

    update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+        poppin: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
+
+		},
 	},
 	plugins: [],
 }
