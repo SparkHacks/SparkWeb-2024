ea745e0bc3d35ae25560e1ad67b84d2b0a1005c2
Hamza G. <hamzagaziuddin04@gmail.com>
2024-01-05 18:31:51 -0600

    maybe fixed gif issue?

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,7 +10,7 @@ export default {
         hyperhelix: ["HyperHelix", ...defaultTheme.fontFamily.sans],
       },
       backgroundImage: {
-        "stars-pattern": "url('sparkle-65.gif')",
+        "stars-pattern": "url('/sparkle-65.gif')",
       },
     },
   },
